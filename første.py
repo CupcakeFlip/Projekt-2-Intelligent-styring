3ab60b7b85944588bd29da0785804374ce6530b7
Tiffany <tiff0081@zbc.dk>
2023-01-30 10:01:22 +0100

    Fået side til at vise live kamera

--- første.py
+++
@@ -1,3 +0,0 @@
-# hello
-# yesyes
-# u ejte
---
+++ websockets/simple.js
@@ -0,0 +1,22 @@
+let count = 0;
+
+function websocket(wss) {
+    wss.on("connection", (ws) => {
+        console.log("Client connected");
+
+        ws.on("message", (data, isBinary) => {
+            data = isBinary ? data : data.toString();
+            console.log(data);
+        });
+
+        ws.on("close", () => console.log("Client disconnected"));
+    });
+
+    setInterval(() => {
+        wss.clients.forEach((client) => {
+            client.send(count++);
+        });
+    }, 1000);
+}
+
+module.exports = websocket;
